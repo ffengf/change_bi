779de6cb670f9840c07389714895d5784134e151
JohnsirK <1241010946@qq.com>
2020-12-16 15:20:22 +0800

    myclub status undown

--- src/views/user/club/index.vue
+++ src/views/user/club/index.vue
@@ -1,15 +1,34 @@
 <template>
-    <div class="warpper">
-		<div class="box">
+    <div class="warpper" v-loading="loading">
+		<div class="box" v-for="(ele) in list" :key="ele.id">
 			<div class="box_left">
 				<h2>
-					<span>진행 중</span>
+					<span v-if="ele.status === 0">审核中</span>
+					<span v-if="ele.status === 1 && ele.club_status === 5">未开始</span>
+					<span v-if="ele.status === 1 && ele.club_status === 1">进行中</span>
+					<span v-if="ele.status === 1 && ele.club_status === 2">已结束</span>
+					<span v-if="ele.status === 2">已取消</span>
 					<span>|</span>
-					<span>2020. 11. 01 ~ 2020. 11. 30</span>
+					<span>{{ ele.start_time }} ~ {{ ele.end_time }}</span>
 				</h2>
-				<h1>클럽 창작과비평 2기</h1>
+				<h1>{{ ele.title }}</h1>
 			</div>
-			<el-button class="right_btn" type="primary">참여하기</el-button>
+			<div class="flex" v-if="ele.status === 0 || ele.status === 1 && ele.club_status === 5">
+				<el-button class="right_btn" type="danger">取消申请</el-button>
+				<el-button class="right_btn" type="primary">查看内容</el-button>
+			</div>
+			<div class="flex" v-if="ele.status === 1 && ele.club_status === 1">
+				<el-button class="right_btn" type="primary">去参加</el-button>
+			</div>
+			<div class="flex" v-if="ele.status === 1 && ele.club_status === 2">
+				<el-button class="right_btn" type="primary">查看内容</el-button>
+				<el-button class="right_btn" type="primary">打印结业证</el-button>
+			</div>
+			<div class="flex" v-if="ele.status === 2">
+				<p class="text" v-if="ele.refuse_reason !== null">{{ ele.refuse_reason }}</p>
+				<p class="text" v-else>自己取消</p>
+				<el-button class="right_btn" type="primary">删除</el-button>
+			</div>
 		</div>
 		<el-button class="more" type="success" :disabled="disabled">더 보기</el-button>
     </div>
@@ -35,6 +54,8 @@ export default class extends More(api_user.get_club) {
 
 
 <style lang='less' scoped>
-
-
+.flex{
+	width: 13rem;
+	justify-content: space-between;
+}
 </style>
